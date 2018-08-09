ecc7eee666adb15948be62c6b64ecb839191135e
Pablo Yaniero <pyaniero@theelectricfactory.com>
2018-08-09 10:50:25 -0300

    Initial Commit

---
+++ GPS Precission Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPS Precission Test
+//
+//  Created by Pablo Yaniero on 9/8/18.
+//  Copyright © 2018 Pablo Yaniero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
